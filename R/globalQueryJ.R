aaronwolen/dmGWAS2

commit 294b6805af2cb627ebce30f5a785e172931ad656

globalQueryJ: Simplify and enable parallelization

R/globalQueryJ.R
@@ -1,13 +1,6 @@
-globalQueryJ <-
-function (G, search_r = 1, r = 0.1, lambda=0.5, min.size=5){
-      #system.time({
-	sublist = list()
-	for (node in V(G)$name) {
-		ng <- seedQueryJ(G, node, search_r, r, lambda)
-            #ng <- seedQueryJ(G, node, search_r, r, lambda=0)
-		if (vcount(ng) >= min.size) sublist[[node]] <- ng       #minimum size filtering
-	}
-      #})
-	return(sublist)
+globalQueryJ <- function (G, search_r = 1, r = 0.1, lambda = 0.5, min.size = 5) {
+  sublist <- mclapply(V(G)$name, seedQueryJ, G = G, r = r, mc.cores = .cores())
+  sublist.size <- vapply(sublist2, vcount, FUN.VALUE = numeric(1))
+  sublist[sublist.size >= min.size]
 }
 

DESCRIPTION
@@ -10,6 +10,9 @@ Depends:
     R (>= 2.9.0)
 Imports:
     igraph
+Suggests:
+    foreach,
+    doParallel
 Description: dmGWAS_3.0 implement our upgraded algorithm EW_dmGWAS, which searches for dense modules in a node- and edge-weighted PPI network.
 License: GPL (>= 2)
 URL: http://bioinfo.mc.vanderbilt.edu/dmGWAS

R/parallel.r
@@ -1,0 +1,6 @@
+# return number of cores registered with the foreach package
+.cores <- function(cores = 1) {
+  if (requireNamespace("foreach", quietly = TRUE))
+    cores <- foreach::getDoParWorkers()
+  return(cores)
+}

commit 6a475313496d33136588a18574e77e06473f6f2c

Fix bug introduced in 294b6805

R/globalQueryJ.R
@@ -1,6 +1,6 @@
 globalQueryJ <- function (G, search_r = 1, r = 0.1, lambda = 0.5, min.size = 5) {
   sublist <- mclapply(V(G)$name, seedQueryJ, G = G, r = r, mc.cores = .cores())
-  sublist.size <- vapply(sublist2, vcount, FUN.VALUE = numeric(1))
+  sublist.size <- vapply(sublist, vcount, FUN.VALUE = numeric(1))
   sublist[sublist.size >= min.size]
 }
 
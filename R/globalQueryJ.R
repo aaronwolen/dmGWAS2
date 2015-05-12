6a475313496d33136588a18574e77e06473f6f2c
aaronwolen <aaron@wolen.com>
2015-05-12 14:09:59 -0400

    Fix bug introduced in 294b6805

--- R/globalQueryJ.R
+++ R/globalQueryJ.R
@@ -1,6 +1,6 @@
 globalQueryJ <- function (G, search_r = 1, r = 0.1, lambda = 0.5, min.size = 5) {
   sublist <- mclapply(V(G)$name, seedQueryJ, G = G, r = r, mc.cores = .cores())
-  sublist.size <- vapply(sublist2, vcount, FUN.VALUE = numeric(1))
+  sublist.size <- vapply(sublist, vcount, FUN.VALUE = numeric(1))
   sublist[sublist.size >= min.size]
 }
 
