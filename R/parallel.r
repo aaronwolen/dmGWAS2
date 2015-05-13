df2b2658583fd473305a56eb70defced0077b008
aaronwolen <aaron@wolen.com>
2015-05-13 10:51:34 -0400

    Move .cores() utils.R

--- R/parallel.r
+++
@@ -1,6 +0,0 @@
-# return number of cores registered with the foreach package
-.cores <- function(cores = 1) {
-  if (requireNamespace("foreach", quietly = TRUE))
-    cores <- foreach::getDoParWorkers()
-  return(cores)
-}
--- R/utils.R
+++ R/utils.R
@@ -27,3 +27,11 @@ deduplicate <- function(x) {
   ids <- vapply(x, digest::digest, algo = "md5", FUN.VALUE = character(1))
   x[!duplicated(ids)]
 }
+
+
+# return number of cores registered with the foreach package
+.cores <- function(cores = 1) {
+  if (requireNamespace("foreach", quietly = TRUE))
+    cores <- foreach::getDoParWorkers()
+  return(cores)
+}
